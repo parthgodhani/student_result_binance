f26c5db8d0f1def1a728a266ed533c47213f9410
Chandresh K <chandresh.kathiriya93@gmail.com>
2022-02-02 13:56:30 +0530

    test

--- contracts/StudentResult.sol
+++ contracts/StudentResult.sol
@@ -3,4 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract StudentResult {
     uint256 public studentCount = 0;
+    
+    struct Subject {
+        string key;
+        uint256 marks;
+    }
+    struct Student {
+        uint256 id;
+        string name;
+        Subject[] subjects;
+    }
+
+    mapping(uint256 => Student) public students;
+
+    function createStudent(string memory _name, Subject[] memory subjects)
+        public
+    {
+        students[studentCount] = Student(studentCount, _name, subjects);
+        studentCount++;
+    }
 }
