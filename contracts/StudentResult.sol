90c57b0b27b163e8409ed237cb95ea3a86f8d727
Chandresh K <chandresh.kathiriya93@gmail.com>
2022-02-02 12:34:52 +0530

    project setup

---
+++ contracts/StudentResult.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract StudentResult {
+    uint256 public studentCount = 0;
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const StudentResult = artifacts.require("StudentResult");
+
+module.exports = function (deployer) {
+  deployer.deploy(StudentResult);
+};
